7b93ecadded6fe091ef0c85a2035a018ef8e1928
MaAsche <matthiaskg.asche@gmail.com>
2021-10-19 20:16:31 +0200

    added chmod for run script & java instalation

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
+
 # Setup Backend
 rm -rf backend/build/
 echo "Deleted build/ folder"
 
-backend/./gradlew bootJar
+./backend/gradlew bootJar -p ./backend
 echo "Generating jar file"
 
 #Copy execute_commands_on_ec2.sh file which has commands to be executed on server... Here we are copying this file
@@ -15,8 +16,11 @@ echo "Copied latest 'execute_commands_on_ec2.sh' file from local machine to ec2 
 scp -i "~/Downloads/exercise2.pem" backend/build/libs/backend-0.0.1-SNAPSHOT.jar ubuntu@ec2-52-28-151-166.eu-central-1.compute.amazonaws.com:/home/ubuntu
 echo "Copied jar file from local machine to ec2 instance"
 
-# change permission of execute_commands_on_ec2.sh
-# install java on vm
+# give execute permission to execute_commands_on_ec2
+ssh -i "~/Downloads/exercise2.pem" ubuntu@ec2-52-28-151-166.eu-central-1.compute.amazonaws.com chmod 777 execute_commands_on_ec2.sh
+
+# install java 11
+ssh -i "~/Downloads/exercise2.pem" ubuntu@ec2-52-28-151-166.eu-central-1.compute.amazonaws.com sudo apt-get install openjdk-11-jdk
 
 echo "Connecting to ec2 instance and starting server using java -jar command"
 ssh -i "~/Downloads/exercise2.pem" ubuntu@ec2-52-28-151-166.eu-central-1.compute.amazonaws.com ./execute_commands_on_ec2.sh
