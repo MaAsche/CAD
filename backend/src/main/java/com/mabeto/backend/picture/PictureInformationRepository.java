65c7c196613b6a96923023cb87db1da92ad7c523
SkinnyLemon <43953592+SkinnyLemon@users.noreply.github.com>
2021-10-21 00:44:30 +0200

    Moved pictures to local file system

--- backend/src/main/java/com/mabeto/backend/picture/PictureInformationRepository.java
+++ backend/src/main/java/com/mabeto/backend/picture/PictureInformationRepository.java
@@ -1,11 +1,11 @@
 package com.mabeto.backend.picture;
 
 
-import com.mabeto.backend.picture.model.Picture;
+import com.mabeto.backend.picture.model.PictureInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 @RepositoryRestResource(collectionResourceRel = "picture", path = "picture")
-public interface PictureRepository extends JpaRepository<Picture, Long> {
+public interface PictureInformationRepository extends JpaRepository<PictureInformation, Long> {
 }
--- frontend/src/components/Entry.js
+++ frontend/src/components/Entry.js
@@ -7,7 +7,7 @@ const Entry = ({ entry, onDelete }) => {
       <h3>{entry.description} 
         <FaTimes style={{ color:'red', cursor: 'pointer'}} onClick={() => onDelete(entry.id)}/>
       </h3>
-      <img src={`data:image/jpeg;base64,${entry.image}`} alt="img" width="400" height="400"/>
+      <img src={`/images/${entry.id}`} alt="img" width="400" height="400"/>
     </div>
   )
 }
